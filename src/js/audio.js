93ab201e17abdc3cacae1d1aa1534056cb40e3d8
Mathieu Perez <ghostofpq@gmail.com>
2012-03-24 20:02:11 +0100

    debut de correction du son

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,53 +1,53 @@
 //audio managment
-Crafty.audio.add({
-	bgMusic: ["", // wav
-		"media/ZombieBattleQN.mp3", //map3
-		"media/ZombieBattleQN.ogg"], //ogg
-	signCreate: ["",
-		"media/InvocPoto.mp3",
-		"media/InvocPoto.ogg"],
-	signMove: ["",
-		"media/TournagePoto.mp3",
-		"media/TournagePoto.ogg"],
-	signDelete: ["",
-		"media/EnlevagePoto.mp3",
-		"media/EnlevagePoto.ogg"],
-	signBroken: ["",
-		"media/SignBroken.mp3",
-		"media/SignBroken.ogg"],
-	doorOpen: ["",
-		"media/DoorOpening.mp3",
-		"media/DoorOpening.ogg"],
-	doorClose: ["",
-		"media/DoorClosing.mp3",
-		"media/DoorClosing.ogg"],
-	holeDig: ["",
-		"media/DigHole.mp3",
-		"media/DigHole.ogg"],
-	zombieDie: ["",
-		"media/ZombiDies1.mp3",
-		"media/ZombiDies1.ogg"],
-	zombieSounds: ["",
-		"media/Zombi1.mp3",
-		"media/Zombi1.ogg"],
-	zombieRage: ["",
-		"media/ZombiRage.mp3",
-		"media/ZombiRage.ogg"],
-	fortressAttack: ["",
-		"media/ZombiAttackBase.mp3",
-		"media/ZombiAttackBase.ogg"],
-	soldierDie: ["",
-		"media/SoldierDies.mp3",
-		"media/SoldierDies.ogg"],
-	cityDie: ["",
-		"media/CityDies.mp3",
-		"media/CityDies.ogg"],
-	gameOver: ["",
-		"media/GameOver.mp3",
-		"media/GameOver.ogg"],
-	pause: ["",
-		"media/LaPause.mp3",
-		"media/LaPause.ogg"]
-});
+Crafty.audio.MAX_CHANNEL=1;
+
+Crafty.audio.add("bgMusic", [
+        "media/ZombieBattleQN.mp3", 
+        "media/ZombieBattleQN.ogg"]);
+Crafty.audio.add("signCreate", [
+        "media/InvocPoto.mp3", 
+        "media/InvocPoto.ogg"]);
+Crafty.audio.add("signMove", [
+        "media/TournagePoto.mp3", 
+        "media/TournagePoto.ogg"]);
+Crafty.audio.add("signDelete", [
+        "media/EnlevagePoto.mp3", 
+        "media/EnlevagePoto.ogg"]);
+Crafty.audio.add("signBroken", [
+        "media/SignBroken.mp3", 
+        "media/SignBroken.ogg"]);
+Crafty.audio.add("doorOpen", [
+        "media/DoorOpening.mp3", 
+        "media/DoorOpening.ogg"]);
+Crafty.audio.add("doorClose", [
+        "media/DoorClosing.mp3", 
+        "media/DoorClosing.ogg"]);
+Crafty.audio.add("holeDig", [
+        "media/DigHole.mp3", 
+        "media/DigHole.ogg"]);
+Crafty.audio.add("zombieDie", [
+        "media/ZombiDies1.mp3", 
+        "media/ZombiDies1.ogg"]);
+Crafty.audio.add("zombieSounds", [
+        "media/Zombi1.mp3", 
+        "media/Zombi1.ogg"]);
+Crafty.audio.add("zombieRage", [
+        "media/ZombiRage.mp3", 
+        "media/ZombiRage.ogg"]);
+Crafty.audio.add("fortressAttack", [
+        "media/ZombiAttackBase.mp3", 
+        "media/ZombiAttackBase.ogg"]);
+Crafty.audio.add("soldierDie", [
+        "media/SoldierDies.mp3", 
+        "media/SoldierDies.ogg"]);
+Crafty.audio.add("cityDie", [
+        "media/CityDies.mp3", 
+        "media/CityDies.ogg"]);
+Crafty.audio.add("gameOver", [
+        "media/GameOver.mp3", 
+        "media/GameOver.ogg"]);
+Crafty.audio.add("pause", [
+        "media/LaPause.mp3", 
+        "media/LaPause.ogg"]);
 
 // Use Crafty.audio.play(String id, number repeat); to play sounds repeat time (-1 => loop)
