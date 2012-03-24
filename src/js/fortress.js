0edfc4e530d840cef37488243a3a9afd4d9d7d75
Florent Lavy <flo.lavy@gmail.com>
2012-03-24 18:32:49 +0100

    modified fortress

--- src/js/fortress.js
+++ src/js/fortress.js
@@ -1,14 +1,11 @@
 Crafty.c('Fortress', {
-	Zombie : function(){
-			this.requires("solid, SpriteAnimation, Collision")
-			.onHit("solid", function () {
-					//Move unit out of solid tile
-			})
-			.bind('Moved', function(from) {
-				if(this.hit('solid')){
-					this.attr({x: from.x, y:from.y});
-			}
-		})
+	hitPoints: ETA.config.game.hitPointsFortress,
+	init: function() {
+		this.requires("2D,DOM");
+		return this;
+	},
+	Fortress : function(){
+
 		return this;
 	}
 	
