733b8400b266d64f34e264b02ee9c6c462df9709
Vincent MOMIN <momobulle@gmail.com>
2012-03-24 16:54:34 +0100

    MAJ Sign

--- src/js/cell.js
+++ src/js/cell.js
@@ -1,12 +1,20 @@
 Crafty.c("Cell", {
     center : {x:0, y:0},
+    elem   : null,
     init: function() {
         this.requires('2D, DOM');
         return this;
     },
     cell : function() {
-		//center= {this.x + this.w/2, this.y + this.h/2};
+		this.center = {x:this._x +25 + this._w/2, y:this._y + 25 + this._h/2};
 		return this;
+	},
+	attribute : function(p) {
+		if(this.elem != p) {
+			this.elem = p;
+			return true;
+		}
+		return false;
 	}
 });
 
