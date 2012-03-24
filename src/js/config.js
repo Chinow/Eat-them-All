9968bc7d5a83ce0f56ee6807c705174f46746cd6
Aurelien Defossez <adefossez@Aureliens-MacBook-Pro.local>
2012-03-24 19:36:03 +0100

    2Players configuration refactored.

--- src/js/config.js
+++ src/js/config.js
@@ -2,19 +2,22 @@
 ETA = window.ETA || {};
 
 ETA.config = {
-	keyset:"zqsd",
-	//keyset:"wasd",
+	// Scene
+	stageWidth: 1000,
+	stageHeight: 580,
+	nbTileWidth: 20,
+	nbTileHeight: 11,
+	
+	// Animation
 	frameRate: 60,
 	zombiAnimationRate:5,
 	dollAnimationRate:10,
 	signBeginAnimationRate:5,
 	signAnimationRate:8,
-	stageWidth: 1000,
-	stageHeight: 580,
-	nbTileWidth: 20,
-	nbTileHeight: 11,
+	
 	cellVoodooDool:null,
 	keyDownSign: Crafty.keys.ENTER,
+	
 	tile:{
 		tileWidth: 50,
 		tileHeight: 50
@@ -26,10 +29,13 @@ ETA.config = {
 	},
 	p1: {
 		startPosition: { x: 150, y: 250, z: 1000 },
+		keyset: "zqsd",
+		//keyset:"wasd",
 		actionKey: Crafty.keys.SPACE
 	},
 	p2: {
 		startPosition: { x: 800, y: 250, z: 1000 },
+		keyset: "arrows",
 		actionKey: Crafty.keys.ENTER
 	}
 };
